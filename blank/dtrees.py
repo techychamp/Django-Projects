7ab7b1a1155ac8d07411fe2b05a14dd076693da5
Yugesh <karunayug@gmail.com>
2019-10-10 22:00:34 +0530

    version 0.23

--- blank/dtrees.py
+++ blank/dtrees.py
@@ -10,12 +10,13 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error,accuracy_score
 from sklearn.tree import export_graphviz
 from sklearn.externals.six import StringIO
 from IPython.display import Image
-import graphviz,nt,pydotplus,pandas as pd
+import graphviz,pydotplus,pandas as pd
 from sklearn import tree as clf
+
 class classifier:
     def __init__(self,file):
         self.out=open("blank/static/output/result.txt","w")
@@ -23,17 +24,15 @@ class classifier:
         self.changer()
         self.process()
         self.export()
-    def loader(self,file):
+    def loader(self,file):#To load file from directory 
         self.file=file
         self.dataset = pd.read_csv("blank/static/tmp/"+self.file)
         self.feature_cols=[i for i in (self.dataset.head(0).keys())[2:]]
         self.X = self.dataset.iloc[:, 2:].values
         self.Y = self.dataset.iloc[:, 1].values
-        #print(self.dataset.head(),file=self.out)
+        
     def changer(self):
-        #print("Cancer data set dimensions : {}".format(self.dataset.shape),file=self.out)
         #Encoding categorical data values
-        #print(self.dataset.isnull().sum(),file=self.out)
         print(self.dataset.isna().sum(),file=self.out)
         self.labelencoder_Y = LabelEncoder()
         self.Y = self.labelencoder_Y.fit_transform(self.Y)
@@ -43,15 +42,16 @@ class classifier:
         self.sc = StandardScaler()
         self.X_train = self.sc.fit_transform(self.X_train)
         self.X_test = self.sc.transform(self.X_test)
-    def process(self):
+    def process(self):#To make a decision tree predict
         self.classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
         self.tree=self.classifier.fit(self.X_train, self.Y_train)
         self.Y_pred = self.tree.predict(self.X_test)
         #To check the accuracy
         self.err=mean_squared_error(self.Y_test, self.Y_pred)
-        print("Total Error:",self.err,file=self.out)
+        self.acc=accuracy_score(self.Y_test, self.Y_pred)
+        print("Total Error:",round(self.err*100,2),"\nAccuracy :",round(self.acc*100,2),file=self.out)
         self.out.close()
-    def export(self):
+    def export(self):#To export decision tree as a dot file  
         self.dot_data = StringIO()
         export_graphviz(self.tree, out_file=self.dot_data,  
                 filled=True, rounded=True,
